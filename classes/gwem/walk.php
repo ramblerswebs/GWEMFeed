45fd7e888c88cac84f116ef9f6ddeb4bbb5e03cf
keithgrimes <keith.grimes@kcgrimes.co.uk>
2023-06-11 15:36:03 +0100

    Updates to display events

--- classes/gwem/walk.php
+++ classes/gwem/walk.php
@@ -18,7 +18,7 @@ class GwemWalk {
     const WM_TIMEFORMAT = "Y-m-d\TH:i:s";
     const GWEM_TIMEFORMAT = "Y-m-d\TH:i:s";
 
-
+    public $type;            // Type whether group-walk or group-event
     public $id;              // id
     public $status;          // status
     public $difficulty;      // difficulty/description
@@ -60,7 +60,134 @@ class GwemWalk {
     public $url;             // url
     public $external_url;    // external URL
 
-    public function __construct($wm_walk) {
+    public function __construct($wm_walk)
+    {
+        $this->type = strtolower($wm_walk->type);
+        switch (strtolower($this->type))
+        {
+            case "group-event":
+                $this->populateGroupEvent($wm_walk);
+                break;
+            default: // Assume it is a group walk.
+                $this->populateGroupWalk($wm_walk);
+                break;
+        }
+    }
+
+    private function populateGroupEvent($wm_event) {
+
+        $this->points = array() ;                      // start_location, meeting_location
+        $this->id = $wm_event->id;
+        $this->title = $wm_event->title;
+        $this->groupCode = $wm_event->group_code;
+        $this->groupName = $wm_event->group_name;
+        $this->description = $wm_event->description ;
+
+        $eventDate = DateTime::createFromFormat(self::WM_TIMEFORMAT, $wm_event->start_date_time);
+        if ($eventDate) {
+            try {  $eventDate->setTime(0,0,0,0); }
+            catch (Exception $e) {$a = $e;}
+            $this->date = $eventDate->format(self::GWEM_TIMEFORMAT);   
+        }
+        $eventFinishTime = DateTime::createFromFormat(self::WM_TIMEFORMAT, $wm_event->end_date_time);
+        if ($eventFinishTime) { $this->finishTime = $eventFinishTime->format('H:i:s');   }                            // finish_date_time
+
+        if ($wm_event->location)
+        {
+            $len = count($this->points);
+            $this->points[$len] = $this->location2point($wm_event->location, "Meeting", $wm_event->start_date_time);
+        }
+
+
+        $this->status = new stdClass();
+        $this->status->value = ($wm_event->status == "confirmed") ? "published" : "cancelled";
+
+        $this->walkContact = new stdClass();
+        $this->walkContact->contact = new stdClass();
+        if ($wm_event->event_organiser != null)
+        {
+            $this->walkContact->contact->displayName = $wm_event->event_organiser->name ;                                    // walk_leaders
+            $this->walkContact->contact->form = $wm_event->event_organiser->email_form;
+            $this->walkContact->contact->telephone1 = ($wm_event->event_organiser->telephone == null) ? "" : $wm_event->event_organiser->telephone;
+            $this->walkLeader = "" ;                              // walk_leaders         
+        }
+        else{
+            $this->walkContact->contact->displayName = "" ;                                    // walk_leaders
+            $this->walkContact->contact->email = "";
+            $this->walkContact->contact->telephone1 = "";
+            $this->walkLeader = "";                              // walk_leaders         
+
+        }
+        $this->walkContact->isWalkLeader = false;
+        $this->walkContact->contact->telephone2 = "";
+        $this->url = $wm_event->url;
+        $this->external_url = $wm_event->external_url;
+
+
+        // Need to default the grade for the UI
+        $this->difficulty = new stdClass();
+        $this->difficulty->text = "Easy Access";
+
+
+        $this->strands = new stdClass();
+        $this->strands->items = array();
+        $this->linkedEvent = new stdClass();
+        $this->linkedEvent->text = "";                          // linked_event
+        $this->festivals = new stdClass();
+        $this->festivals->items = array();
+
+        $this->linkedWalks = new stdClass();
+        $this->linkedWalks->items = array();
+        $this->linkedRoute;
+        $this->additionalNotes = "";
+        $this->distanceKM = null;
+        $this->distanceMiles = null;
+        $this->suitability = new stdClass();
+        $this->suitability->items = array();
+        $this->surroundings = new stdClass();
+        $this->surroundings->items = array();
+        $this->theme = new stdClass();
+        $this->theme->items = array();
+        $this->specialStatus = new stdClass();
+        $this->specialStatus->items = array();
+        $this->facilities = new stdClass();
+        //$this->map_services($wm_event->facilities, $this->facilities);
+        //$this->map_services($wm_event->accessibility, $this->suitability);
+        //$this->map_services($wm_event->transport, $this->facilities);
+        $this->pace = "";
+        $this->ascentMetres = null;
+        $this->ascentFeet = null;
+        $this->gradeLocal = "";
+        $this->attendanceMembers = null;
+        $this->attendanceNonMembers = null;
+        $this->attendanceChildren = null;
+        $this->cancellationReason = $wm_event->cancellation_reason;
+        $dateUpdated = DateTime::createFromFormat(self::WM_TIMEFORMAT, $wm_event->date_updated);
+        if ($dateUpdated) { $this->dateUpdated = $dateUpdated->format("Y-m-d\TH:i:sP"); }
+        $dateCreated = DateTime::createFromFormat(self::WM_TIMEFORMAT, $wm_event->date_created);
+        if ($dateCreated) { $this->dateCreated = $dateCreated->format("Y-m-d\TH:i:sP"); }
+        $this->media = array();
+        $mediacount = 0;
+        if ($wm_event->media != null)
+        {
+            foreach($wm_event->media as $walkmedia)
+            {
+                $this->media[$mediacount] = new stdClass();
+                $this->media[$mediacount]->caption = $wm_event->media[$mediacount]->alt;
+                $this->media[$mediacount]->copyright = "";
+                $pos = strrpos($wm_walk->media[$mediacount]->styles[2]->url,'/', 0);
+                $this->media[$mediacount]->url =  $wm_event->media[$mediacount]->styles[2]->url;
+                $this->media[$mediacount]->fileName = substr($wm_event->media[$mediacount]->styles[2]->url, $pos+1);
+                $mediacount++;
+            }
+        }
+
+        $this->groupInvite = new stdClass(); 
+        $this->groupInvite->groupCode = null;       // groups_invited   ??
+        $this->isLinear = false; 
+    }
+
+    private function populateGroupWalk($wm_walk) {
 
         $this->id = $wm_walk->id;
         $this->status = new stdClass();
